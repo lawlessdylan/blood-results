3eb985f381d002ebcd9fc03ba1ef6ca6fc1ee2e8
lawlessdylan <44977789+lawlessdylan@users.noreply.github.com>
2020-03-31 16:30:08 +0100

    hello

--- src/components/App.js
+++ src/components/App.js
@@ -1,98 +1,219 @@
 /**
  * @Author: dylanlawless
- * @Date:   2019-11-07T12:38:26+00:00
+ * @Date:   2020-01-15T09:52:42+00:00
  * @Last modified by:   dylanlawless
- * @Last modified time: 2020-03-31T16:24:52+01:00
+ * @Last modified time: 2020-03-31T16:30:03+01:00
  */
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+import './App.css';
+
+import MyNav from './Nav';
+import Home from './patient/Home';
+import Tests from './patient/Tests';
+
+import TestResults from './patient/TestResults';
+
+import Results from './patient/Results';
+import ResultsChart from './patient/ResultsChart';
 
 
-import React, {useState} from 'react';
-import Modal from 'react-bootstrap/Modal';
-import { Button } from 'react-bootstrap';
+import Signup from './auth/Signup';
+import Signup2 from './auth/Signup2';
+import Login from './auth/Login';
+
+import EditUser from './patient/EditUser';
+
+import DoctorTestOrders from './doctor/DoctorTestOrders';
+import CreateThyroid from './doctor/CreateThyroid';
+import CreateHa1bc from './doctor/CreateHa1bc';
+import CreateVitaminB12 from './doctor/CreateVitaminB12';
+
 import axios from 'axios';
 
- import '../styles/tests.css';
 
 
- function ConfirmOrderModal(props) {
-
-   const [show, setShow] = useState(false);
-
-   const handleClose = () => {
-
-     setShow(false);
-
-   };
-   const handleShow = () => {
-     setShow(true);
 
 
-};
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+
+} from "react-router-dom";
+
+const BrowserHistory = require('browser-history').default;
 var CryptoJS = require("crypto-js");
 
-const testOrder = {
-    patient_id:  props.patient_id,
-    test_id: props.test_id,
-    doctor_id: props.doctor_id,
-    date: new Date(),
-    status: "Pending"
+class App extends React.Component{
 
- }
+    constructor(props) {
+        super(props);
 
-const sendTestOrder = () => {
+        this.state= {
 
+            loggedIn: localStorage.getItem('jwtToken') !== null,
+            user:{
+                name: '',
+                info: {    honorific: ''}
 
-    axios.defaults.headers.common['Authorization'] = localStorage.getItem('jwtToken')
-    axios.post('http://localhost:5000/testOrder', testOrder)
-      .then(res => {
-          console.log(res.data);
-          handleClose()
-      })
-      .catch(err => {
-          console.log(err);
-      });
-};
-   return (
+            },
 
-     <div>
+            isDoctor: false,
+            doctorName: '',
+            page: window.location,
+        };
 
+    }
+    whatPage = () => {
+        this.setState((state, props) => ({
+            page: state.page
+        }));
+    }
 
-
-
-                                {(props.isLoggedIn) ? (
-                                    <Button className="nav-button card-button" type="submit" onClick={handleShow}>
-                                        Order Test
-                                     </Button>
-                                         ) : (
-
-                                        <></>
-                                         )}
-
-       <Modal className="my-modal" show={show} onHide={handleClose} aria-labelledby="contained-modal-title-vcenter" centered>
-
-         <Modal.Header closeButton>
-
-         <Modal.Title>Confirm order</Modal.Title>
-         </Modal.Header>
-         <Modal.Body>
-
-         <div>Are you sure you want to order this test?</div>
-
-         </Modal.Body>
+    authHandler = () => {
+        this.setState((state, props) => ({
+            loggedIn: state.loggedIn ? false : true
+        }));
 
 
-         <Modal.Footer>
-         <Button variant="primary" className="nav-button cancel-modal" onClick={handleClose}>
-          Cancel
-         </Button>
-             <Button onClick={sendTestOrder} target="blank"className="nav-button" variant="primary" type="submit">
-              Order test
-             </Button>
-         </Modal.Footer>
-       </Modal>
+    }
 
-    </div>
+     UNSAFE_componentWillMount() { //unsafe warning in console
+
+        const loggedIn = this.state.loggedIn;
+
+        if(loggedIn === true){
+            const token =  localStorage.getItem("jwtToken")
+            const tokenParts = token.split(".")
+            const encodedPayLoad = tokenParts[1]
+            const rawPayLoad = atob(encodedPayLoad)
+            const user = JSON.parse(rawPayLoad)
+            this.setState({
+
+              user: user
+
+
+            })
+
+
+            if(user.role.role_name === "Doctor"){
+                // console.log("User is a Doctor")
+                this.setState((state, props) => ({
+                    isDoctor: true,
+                    doctorName: user.name
+                }));
+
+                } else {
+                    console.log("User has a user role");
+                    this.setState((state, props) => ({
+                        isDoctor: false
+                    }));
+                }
+            }
+
+
+
+        };
+
+        componentDidMount(){
+            const loggedIn = this.state.loggedIn;
+            if(loggedIn === true){
+                axios.get('http://localhost:5000/user/' + this.state.user._id)
+                .then(response => {
+
+
+                    const encryptedName = CryptoJS.AES.decrypt(response.data.name.toString(), response.data.password)
+                    const decryptedName = encryptedName.toString(CryptoJS.enc.Utf8);
+
+                  this.setState({
+                    user : {
+                        name: decryptedName,
+                        _id: response.data._id,
+                        info: {
+
+                            honorific: response.data.info.honorific
+                        }
+                    }
+                  })
+                  // console.log(response);
+
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+            }
+
+        }
+
+
+
+
+
+render(){
+
+    const isDoctor = this.state.isDoctor;
+    const doctorName = this.state.doctorName;
+    // console.log(isDoctor);
+    const loggedIn = this.state.loggedIn;
+    const user = this.state.user;
+    const page = this.state.page;
+
+    return(
+        <Router history={BrowserHistory}>
+            <MyNav doctorName={doctorName} user={user} page={page}  isDoctor={isDoctor} loggedIn={loggedIn} onLogout={this.authHandler} />
+            <Switch>
+                <Route path="/login"  component={Login} />
+                <Route user={user} path="/signup"  component={Signup} />
+                <Route user={user} path="/signup2"  component={Signup2} />
+
+                {(isDoctor) ? (
+                    <Route path="/home"  component={DoctorTestOrders}>
+                        {isDoctor ? <DoctorTestOrders doctorName={doctorName} user={user} /> : <Redirect to="/home" />}
+                    </Route>
+
+                ):(
+                    <Route path="/home"  component={TestResults}>
+                          <TestResults isDoctor={isDoctor} loggedIn={loggedIn} user={user}/>
+                    </Route>
+                )}
+
+              <Route exact path="/editUser" component={EditUser} >
+                      {loggedIn ? <EditUser user={user} /> : <Redirect to="/editUser" />}
+              </Route>
+              <Route path="/tests"  component={Tests}>
+                  {loggedIn ? <Tests user={user}/> : <Redirect to="/login" />}
+              </Route>
+
+
+              <Route path="/results"  component={Results}>
+                  {loggedIn ? <Results isDoctor={isDoctor} user={user} /> : <Redirect to="/login" />}
+              </Route>
+
+              <Route path="/resultsChart"  component={ResultsChart}>
+                  {loggedIn ? <ResultsChart user={user} /> : <Redirect to="/login" />}
+              </Route>
+
+
+
+
+                {isDoctor ? <Route path="/createThyroid/:id" component={CreateThyroid} /> : <Redirect to="/home" />}
+               {isDoctor ? <Route path="/createHa1bc/:id" component={CreateHa1bc} /> : <Redirect to="/home" />}
+               {isDoctor ? <Route path="/createVitaminB12/:id" component={CreateVitaminB12} /> : <Redirect to="/home" />}
+
+
+
+
+
+
+              </Switch>
+        </Router>
+
+
+
    );
  }
-
- export default ConfirmOrderModal;
+}
+ export default App;
